1ab594f0a31fff082b56050e2bbb2e77c7518955
Omar Angarita <oangarita@pibox.app>
2022-10-22 11:28:58 -0500

    add dependency csv-parser

--- src/seed/services/seedService.ts
+++ src/seed/services/seedService.ts
@@ -1,3 +1,7 @@
+import * as fs from "fs";
+import * as path from "path";
+import {parse} from 'csv-parse';
+
 import {BaseService} from "../../config/base.service";
 import {SeedEntity} from "../entities/category.entity";
 
@@ -6,8 +10,20 @@ export class SeedService extends BaseService<SeedEntity> {
     super(SeedEntity);
   }
 
-  async loadCsvFile(): Promise<SeedEntity[]> {
-    return (await this.execRepository).find();
+  loadCsvFile(): void {
+    const csvFilePath = path.resolve(__dirname, '../books.csv');
+    const headers = ["ISBN", "Book-Title", "Book-Author", "Year-Of-Publication", "Publisher", "Image-URL-S", "Image-URL-M", "Image-URL-L"];
+    const fileContent = fs.readFileSync(csvFilePath, {encoding: 'utf-8'});
+
+    parse(fileContent, {
+      delimiter: ',',
+      columns: headers,
+    }, (error, result: WorldCity[]) => {
+      if (error) {
+        console.error(error);
+      }
+      console.log("Result", result);
+    });
   }
 
   async findCategoryById(id: string): Promise<SeedEntity | null> {
